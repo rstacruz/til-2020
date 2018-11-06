3f951e5e511d6e16820ec4521f0e96bbebdec649
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-06 18:16:18 +0800

    Add eslint and update packages

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true
+    // node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2018,
+
+    // Allows 'import' and 'export'
+    sourceType: 'module'
+
+    // ALlows JSX syntax for React
+    ecmaFeatures: { jsx: true }
+  }
+
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: '16.5.0'
+    }
+  },
+  rules: {
+    'react/prop-types': 0
+  },
+
+  Enables parser for Flow annotatinos
+  parser: 'babel-eslint',
+}
