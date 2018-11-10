469b7e0e20da0384bc1b8fb3d31e1ea0eb63b467
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-11 05:39:43 +0800

    Add Waypoint magic

--- src/components/H2Section.js
+++ src/components/H2Section.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import Waypoint from 'react-waypoint'
 import './H2Section.css'
 
-const H2Section = ({ children, className }) => (
-  <>
-    <section className={className}>{children}</section>
-    <div className="h2-separator" />
-  </>
-)
+class H2Section extends React.Component {
+  state = {
+    entered: false,
+  }
+
+  onEnter = () => {
+    this.setState({ entered: true })
+  }
+
+  onLeave = () => {
+    this.setState({ entered: false })
+  }
+
+  render() {
+    const className = [
+      this.props.className,
+      this.state.entered ? '-active' : '-inactive',
+    ].join(' ')
+
+    const { children } = this.props
+
+    return (
+      <Waypoint
+        onEnter={this.onEnter}
+        onLeave={this.onLeave}
+        topOffset="20%"
+        bottomOffset="80%"
+      >
+        <section className={className}>{children}</section>
+      </Waypoint>
+    )
+  }
+}
 
 export default H2Section
