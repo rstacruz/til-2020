5fe806fd7caeadefb72f6e8687da4d1b4c569e43
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-11 21:41:31 +0800

    Improve support for arm64 machines

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
+function supportsManifest() {
+  try {
+    return require.resolve('gatsby-plugin-manifest')
+  } catch (e) {
+    return false
+  }
+}
+
 module.exports = {
   pathPrefix: '/tilnext',
   siteMetadata: {
-    title: 'TIL',
+    title: 'TIL'
   },
   plugins: [
     'gatsby-plugin-postcss',
@@ -10,33 +18,37 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/posts`,
-      },
+        path: `${__dirname}/posts`
+      }
     },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: ['gatsby-remark-component'],
-      },
+        plugins: ['gatsby-remark-component']
+      }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-  ],
+    ...(supportsManifest()
+      ? [
+          {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: 'gatsby-starter-default',
+              short_name: 'starter',
+              start_url: '/',
+              background_color: '#663399',
+              theme_color: '#663399',
+              display: 'minimal-ui',
+              icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+            }
+          }
+        ]
+      : [])
+  ]
 }
