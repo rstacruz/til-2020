ff965c705fd79dff7ee23b77fb44d7a96cdbaa5c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-11-25 00:30:18 +0800

    Add react-typist

--- src/components/BlogPostTitle.js
+++ src/components/BlogPostTitle.js
@@ -3,6 +3,7 @@ import PostContent from './PostContent'
 import CardWaypoint from './CardWaypoint'
 import CSS from './BlogPostTitle.module.css'
 import cn from 'classnames'
+import Typist from 'react-typist'
 
 const BlogPostTitle = ({ title, date, body }) => {
   return (
@@ -12,9 +13,14 @@ const BlogPostTitle = ({ title, date, body }) => {
           <div
             className={cn(CSS.root, entered ? CSS.isActive : CSS.isInactive)}
           >
-            <h1 className={CSS.title}>
-              {title} <span className={CSS.blinker} />
-            </h1>
+            <div className={CSS.titleContainer}>
+              <span className={CSS.titlePlaceholder}>{title}</span>
+
+              <h1 className={CSS.title}>
+                <Typist cursor={{ show: false }}>{title}</Typist>
+                <span className={CSS.blinker} />
+              </h1>
+            </div>
 
             <p className={CSS.byline}>Written by Rico Sta. Cruz / {date}</p>
 
