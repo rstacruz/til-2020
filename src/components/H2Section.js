cc777cfeba6519121e27002adf7435187c751540
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-12-13 11:05:35 +0800

    Add slugs

--- src/components/H2Section.js
+++ src/components/H2Section.js
@@ -4,6 +4,7 @@ import * as React from 'react'
 import CardWaypoint, { type State } from './CardWaypoint'
 import CSS from './H2Section.module.css'
 import cn from 'classnames'
+import slugger from 'github-slugger'
 
 export type Props = {
   children: React.Node,
@@ -15,15 +16,26 @@ const H2Section = ({ children, className }: Props) => {
     <CardWaypoint>
       {({ entered }: State) => {
         const activeClass = entered ? '-active' : '-inactive'
+        const title = getH2Text(children)
+        const id = title ? slugger().slug(title) : null
 
         return (
-          <section className={cn(CSS.root, className, activeClass)}>
+          <section className={cn(CSS.root, className, activeClass)} id={id}>
             {children}
           </section>
         )
       }}
     </CardWaypoint>
   )
+}
+
+function getH2Text(children: React.Node): ?string {
+  if (!Array.isArray(children)) return
+
+  const h2 = children.find(node => node.type === 'h2')
+  if (!h2) return
+
+  return ((h2 && h2.props && h2.props.children) || []).join('')
 }
 
 export default H2Section
