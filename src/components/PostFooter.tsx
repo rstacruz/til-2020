d16610ca9ebc0650c904913c7fac23d096436561
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-30 15:04:09 +0800

    Improve aesthetics

--- src/components/PostFooter.tsx
+++ src/components/PostFooter.tsx
@@ -11,30 +11,22 @@ export interface Props {
 const PostFooter = (props: Props) => {
   const { title, date } = props
   return (
-    <CardWaypoint bottomOffset='50%' topOffset='0%'>
-      {({ entered }: State) => {
-        const activeClass = entered ? '-active' : '-inactive'
-        return (
-          <div className={cn(CSS.root, activeClass)}>
-            <div className={CSS.body}>
-              <h3>Thank you for reading</h3>
-              <p>
-                {date ? (
-                  <>
-                    You have just read <em>{title}</em>, written on{' '}
-                    <em>{date}</em>.
-                  </>
-                ) : (
-                  <>
-                    You have just read <em>{title}</em>.
-                  </>
-                )}
-              </p>
-            </div>
-          </div>
-        )
-      }}
-    </CardWaypoint>
+    <div className={cn(CSS.root, '-active')}>
+      <div className={CSS.body}>
+        <p>
+          {date ? (
+            <>
+              You have just read <em>{title}</em>, written on <em>{date}</em>.
+            </>
+          ) : (
+            <>
+              You have just read <em>{title}</em>, which is an unpublished
+              draft.
+            </>
+          )}
+        </p>
+      </div>
+    </div>
   )
 }
 
