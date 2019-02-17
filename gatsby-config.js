3cc28eeed64e238b264615f0191513864491fef7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-02-17 22:26:48 +0800

    Add 2019 posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
       }
     },
     fs({ name: `posts`, path: `${__dirname}/posts/drafts` }),
+    fs({ name: `posts`, path: `${__dirname}/posts/2019` }),
     fs({ name: `posts`, path: `${__dirname}/posts/2018` }),
     fs({ name: `posts`, path: `${__dirname}/posts/2017` }),
     fs({ name: `posts`, path: `${__dirname}/posts/2016` }),
