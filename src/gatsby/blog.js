c4f33d95ecf7110ab4701c815cd6befbbabe282d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-03-21 17:44:32 +0800

    Omit trailing slashes

--- src/gatsby/blog.js
+++ src/gatsby/blog.js
@@ -20,7 +20,12 @@ const onCreateNode = (ctx /*: any */, { node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({
+      node,
+      getNode,
+      trailingSlash: false
+    })
+
     createNodeField({
       name: 'slug',
       node,
