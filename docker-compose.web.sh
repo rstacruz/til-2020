7f27bfbc93d9cb9c3aad075f1e118985f82ece31
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-04 19:53:09 +0800

    Update docker-compose entry point

--- docker-compose.web.sh
+++ docker-compose.web.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env sh
-rm -rf .cache/*
+
+# Yarn install as needed
+if [ ! -x ./node_modules/.bin/gatsby ]; then
+  yarn
+fi
+
+# Clear cache as needed
+if [ -d .cache ]; then
+  rm -rf .cache/*
+fi
+
+# Yarn run
 yarn run gatsby develop -- --host 0.0.0.0 --port 25800
