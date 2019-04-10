a0b796b57cb4e0c2670272eaaa5cac8acdabcf1c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-11 00:02:45 +0800

    Add canonical URL to OG

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -56,16 +56,15 @@ class BlogPostTemplate extends React.Component<Props> {
           <title>{title}</title>
 
           <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:site' content={siteName} />
-          <meta name='twitter:title' content={title} />
-          <meta name='twitter:description' content={description} />
           <meta name='twitter:image' content={image} />
+          <meta name='twitter:creator' content={'@rstacruz'} />
 
           <meta property='og:type' content='article' />
           <meta property='og:title' content={title} />
           <meta property='og:image' content={image} />
           <meta property='og:description' content={description} />
           <meta property='og:site_name' content={siteName} />
+          <meta property='og:url' content={absurl} />
 
           <meta name='description' content={description} />
         </Helmet>
