bb6431682172d2cef5984f7af9465968e8d41385
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-06-09 04:31:33 +0800

    Extract BackIcon to its own component

--- src/components/MainHeading.tsx
+++ src/components/MainHeading.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames'
 import { Link } from 'gatsby'
 import React, { useState } from 'react'
 import { Waypoint } from 'react-waypoint'
+import BackIcon from './BackIcon'
 import CSS from './MainHeading.module.css'
 
 interface State {
@@ -59,20 +60,3 @@ const doHandleEnter = ({ setState }) => () => {
 const doHandleLeave = ({ setState }) => () => {
   setState({ visible: false })
 }
-
-const BackIcon = () => (
-  <svg
-    version='1.1'
-    width='36'
-    height='36'
-    viewBox='0 0 36 36'
-    preserveAspectRatio='xMidYMid meet'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <path
-      d='M27.66,15.61,18,6,8.34,15.61A1,1,0,1,0,9.75,17L17,9.81V28.94a1,1,0,1,0,2,0V9.81L26.25,17a1,1,0,0,0,1.41-1.42Z'
-      className='clr-i-outline clr-i-outline-path-1'
-    />
-    <rect x='0' y='0' width='36' height='36' fillOpacity='0' />
-  </svg>
-)
