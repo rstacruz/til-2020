45664fa679358c2167ff4fe0cab1b665648bd029
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-01-31 01:18:00 +1100

    Add byline

---
+++ src/simple/lib/ByLine.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CSS from './ByLine.module.css'
+
+interface Props {
+  date: string
+}
+
+const ByLine = (props: Props) => {
+  const { date } = props
+
+  return (
+    <div className={CSS.root}>
+      Rico Sta. Cruz
+      <span className={CSS.sep} />
+      {date ? <span>{date}</span> : <span>Unpublished</span>}
+    </div>
+  )
+}
+
+export default ByLine
