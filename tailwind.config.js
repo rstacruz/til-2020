2d10204db47b513daea3593e17a39e907916364b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-02-01 16:37:31 +1100

    Update styles, put colors in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 // https://tailwindcss.com/docs/configuration/
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        a: {
+          '500': '#705075'
+        },
+        n: {
+          mute: '#5b687a',
+          faded: '#718096',
+          line: '#cbd5e0'
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
