52d0823ea750c3c45fca18185743b83b629733f9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-02-10 23:14:16 +1100

    Migrate some helpers to tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 // https://tailwindcss.com/docs/configuration/
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   theme: {
     extend: {
@@ -19,5 +21,34 @@ module.exports = {
     }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    plugin(({ addUtilities, theme }) => {
+      addUtilities({
+        '.thin-scrollbar': {
+          scrollbarWidth: 'thin',
+          scrollbarColor: `${theme('colors.gray.500')} transparent`
+        },
+
+        '.shadow-8': {
+          boxShadow: [
+            `0 1px 1px rgba(0, 0, 80, 0.05)`,
+            `0 2px 2px rgba(0, 0, 80, 0.05)`,
+            `0 4px 4px rgba(0, 0, 80, 0)`,
+            `0 8px 8px rgba(0, 0, 80, 0)`,
+            `0 16px 16px rgba(0, 0, 80, 0)`
+          ].join(', ')
+        },
+
+        '.shadow-32': {
+          boxShadow: [
+            `0 2px 1px rgba(0, 0, 80, 0.08)`,
+            `0 3px 2px rgba(0, 0, 80, 0.08)`,
+            `0 6px 4px rgba(0, 0, 80, 0.08)`,
+            `0 12px 8px rgba(0, 0, 80, 0.08)`,
+            `0 24px 16px rgba(0, 0, 80, 0.08)`
+          ].join(', ')
+        }
+      })
+    })
+  ]
 }
