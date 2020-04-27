a71cb93a59d38864ce08b973e7b88c2934335f64
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-04-27 23:46:40 +1000

    Refactor config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,30 @@
-const TITLE = 'New Site'
-const SHORT_TITLE = 'My new web site'
-const THEME_COLOR = '#101018'
+const config = () => ({
+  siteMetadata: metadata(),
+  plugins: plugins(),
+})
 
-module.exports = {
-  siteMetadata: {
-    title: TITLE,
-  },
-  plugins: [
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
-    { resolve: 'gatsby-plugin-postcss' },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
-    fs({ name: 'posts', path: `${__dirname}/posts/2020` }),
-    {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: ['.md', '.mdx'],
-      },
-    },
-    manifest(),
-  ],
-}
-
-function fs(options) {
-  return { resolve: 'gatsby-source-filesystem', options }
-}
-
-function manifest() {
-  return {
-    resolve: 'gatsby-plugin-manifest',
+const plugins = () => [
+  'gatsby-plugin-typescript',
+  'gatsby-plugin-react-helmet',
+  { resolve: 'gatsby-plugin-postcss' },
+  {
+    resolve: 'gatsby-source-filesystem',
     options: {
-      name: TITLE,
-      short_name: SHORT_TITLE,
-      start_url: '/',
-      background_color: THEME_COLOR,
-      theme_color: THEME_COLOR,
-      display: 'minimal-ui',
-      icon: 'src/images/icon.png',
+      name: 'images',
+      path: `${__dirname}/src/images`,
     },
-  }
-}
+  },
+  fs({ name: 'posts', path: `${__dirname}/posts/2020` }),
+  {
+    resolve: 'gatsby-plugin-mdx',
+    options: {
+      extensions: ['.md', '.mdx'],
+    },
+  },
+]
+
+const metadata = () => ({ title: 'TIL' })
+
+const fs = (options) => ({ resolve: 'gatsby-source-filesystem', options })
+
+module.exports = config()
