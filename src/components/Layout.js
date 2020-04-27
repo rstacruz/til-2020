aef1a2c7c12ce5f0e1d0c96e35371bdd755a8105
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-04-27 22:24:01 +1000

    Initial commit from gatsby: (https://github.com/rstacruz/gatsby-starter-simplified.git)

---
+++ src/components/Layout.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import 'sanitize.css'
+
+const useSiteMetadata = () => {
+  const query = graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `
+  return useStaticQuery(query).site.siteMetadata
+}
+
+const Layout = ({ children }) => {
+  const siteMetadata = useSiteMetadata()
+
+  return (
+    <>
+      <Helmet
+        title={siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' }
+        ]}
+      >
+        <html lang='en' />
+      </Helmet>
+      {/*<h1>{siteMetadata.title}</h1>*/}
+      <div>{children}</div>
+    </>
+  )
+}
+
+export default Layout
