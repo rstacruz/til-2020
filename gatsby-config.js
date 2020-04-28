d0f43e9cd252268e70c959df57f17a6936ab6131
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-04-28 22:53:28 +1000

    Add proper rehype plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ const plugins = () => [
     resolve: 'gatsby-plugin-mdx',
     options: {
       extensions: ['.md', '.mdx'],
+      rehypePlugins: [require('@rstacruz/rehype-sectionize').plugin],
     },
   },
 ]
--- src/templates/blog-post/MyMDXProvider.tsx
+++ src/templates/blog-post/MyMDXProvider.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import PreCode from '../../simple/lib/PreCode'
+import H2Section from '../../simple/lib/H2Section'
 
 const components = {
   pre: PreCode,
+  section: H2Section,
 }
 
 const MyMDXProvider = ({ children }: { children: React.ReactNode }) => (
