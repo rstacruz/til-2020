85cd7e75f9a7e5c4083e50a62e140b6239e49e38
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-04-30 13:34:40 +1000

    Get rid of everything lol

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (ctx) => {
-  return {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('postcss-preset-env')({ stage: 0 }),
-    ],
-  }
-}
--- src/simple/lib/MarkdownStyles.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import CSS from './MarkdownStyles.module.css'
-
-const MarkdownStyles = ({ children }: { children: React.ReactNode }) => {
-  return <div className={CSS.root}>{children}</div>
-}
-
-export default MarkdownStyles
