b2ed3c5e6b039e7f9cd59d868c55aa466ae04b28
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-05-20 21:34:24 +1000

    Redo the blog nav

--- src/simple/lib/BlogNav.tsx
+++ src/simple/lib/BlogNav.tsx
@@ -10,39 +10,24 @@ interface Props {
 }
 
 const BlogNav = (props: Props) => {
+  /* const { title } = props */
   const { Wrapper, isFixed } = useNavWaypoint()
-  const { title } = props
+  const expanded = isFixed ? CSS.isCollapsed : CSS.isExpanded
 
   return (
     <>
       <Wrapper>
-        <span className={CSS.placeholder} />
+        <div className={CSS.placeholder} />
       </Wrapper>
 
-      <div
-        className={cn(CSS.root, {
-          [CSS.isFixed]: isFixed,
-        })}
-      >
-        <span className={CSS.left}>
-          <Link to='/'>
-            <BackIcon className={CSS.backIcon} />
-          </Link>
-        </span>
+      <div className={cn(CSS.topRight, expanded)}>
+        <span>Today I learned</span>
+      </div>
 
-        <span className={CSS.center}>
-          {isFixed ? (
-            <>
-              <Link to='/'>TIL</Link>
-              <span>
-                <span className={CSS.separator}>/</span>
-                <span className={CSS.title}>{title}</span>
-              </span>
-            </>
-          ) : (
-            <Link to='/'>Today I Learned</Link>
-          )}
-        </span>
+      <div className={cn(CSS.topLeft, expanded)}>
+        <Link to='/' className={CSS.backLink}>
+          <BackIcon className={CSS.backIcon} />
+        </Link>
       </div>
     </>
   )
@@ -62,7 +47,7 @@ const useNavWaypoint = () => {
   const Wrapper = useMemo(
     () => (props: { children: React.ReactNode }) => {
       return (
-        <Waypoint {...{ onEnter, onLeave }}>
+        <Waypoint topOffset='-200px' {...{ onEnter, onLeave }}>
           <span>{props.children}</span>
         </Waypoint>
       )
