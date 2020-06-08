4176608f3f97c7da26ed8c55c272c909375e742f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-06-08 16:04:54 +1000

    Update feed metadata

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,32 +1,29 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+const gql = String.raw
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
-  const result = await graphql(
-    `
-      {
-        allMdx(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-              body
+  const result = await graphql(gql`
+    {
+      allMdx(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+        edges {
+          node {
+            fields {
+              slug
             }
+            frontmatter {
+              title
+            }
+            body
           }
         }
       }
-    `
-  )
+    }
+  `)
 
   if (result.errors) {
     throw result.errors
@@ -56,9 +53,9 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
   if (node.internal.type === 'Mdx') {
     const value = createFilePath({ node, getNode }).replace(/\/$/, '')
-    createNodeField({ name: `slug`, node, value })
+    createNodeField({ name: 'slug', node, value })
     createNodeField({
-      name: `book`,
+      name: 'book',
       node,
       value: node.frontmatter.book || 'notes',
     })
