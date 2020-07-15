58bc6b833c6926524eabc8ddfcff741db1dd1660
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2020-07-16 01:12:03 +1000

    Reorganise CSS imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
+const { resolve } = require('path')
+
 module.exports = (ctx) => {
   return {
     plugins: [
-      require('postcss-import'),
+      require('postcss-import')({
+        path: resolve(__dirname, 'src'),
+      }),
       require('tailwindcss'),
       require('postcss-preset-env')({ stage: 0 }),
     ],
